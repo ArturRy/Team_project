cd0785ef0128031c5f0f485e9439c22dea3ef4fc
ArtemB118 <1evkakii1@gmail.com>
2023-09-22 23:56:36 +0600

    add_vk_ban_list

--- vk_api_team.py
+++ vk_api_team.py
@@ -1,12 +1,15 @@
 import requests
 import vk_api
 import json
+import random
+import json
 
 with open ('VK_token.txt', 'r') as file:
     vk_token = file.readline()
 with open ('service_toke.txt', 'r') as file:
     service_token = file.readline()
 
+plack_list = []
 
 def get_bot_user_info(token: str) -> dict:
     try:
@@ -33,7 +36,6 @@ def get_bot_user_info(token: str) -> dict:
             }
         else:
             print(f"Ошибка при выполнении запроса: {response.status_code}")
-            return 1
     except Exception as e:
         print(f"Ошибка при получении информации о пользователе: {e}")
         return e
@@ -57,6 +59,7 @@ def get_vk_profile_photos(user_id: int, vk_token: str, count=999) -> list:
     return photos
 
 def search_users(min_age: int, max_age: int, city: int, sex: int) -> str:
+    user_list = []
     params = {
         'age_from': min_age,
         'age_to': max_age,
@@ -71,11 +74,47 @@ def search_users(min_age: int, max_age: int, city: int, sex: int) -> str:
         data = response.json()
         users = data['response']['items']
         for user in users:
-            return print(f"Id: {user['id']} Имя: {user['first_name']}, Фамилия: {user['last_name']}")
+            user_info = {
+                'id': user['id'],
+                'first_name': user['first_name'],
+                'last_name': user['last_name'],
+            }
+            user_list.append(user_info)
 
     else:
         print("Ошибка при выполнении запроса")
 
-search_users(22, 27, 3, 2)
-get_vk_profile_photos(559270241, vk_token)
-get_bot_user_info(vk_token)
+    return user_list
+
+def random_users(min_age: int = random.randint(18, 30), max_age: int = random.randint(31, 60), city: int = random.randint(1, 7), sex: int = random.randint(1, 2)) -> list:
+    user_list = []
+    params = {
+        'age_from': min_age,
+        'age_to': max_age,
+        'city': city,
+        'sex': sex,
+        'access_token': vk_token,
+        'v': '5.131',
+        'count': 1,
+    }
+
+    response = requests.get('https://api.vk.com/method/users.search', params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        users = data['response']['items']
+        for user in users:
+            user_info = {
+                'id': user['id'],
+                'first_name': user['first_name'],
+                'last_name': user['last_name'],
+            }
+            user_list.append(user_info)
+
+    else:
+        return response.status_code
+
+    return user_list
+
+def black_list(id_user: int):
+    plack_list.append(id_user)
